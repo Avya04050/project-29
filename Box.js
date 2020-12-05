f10edfe911d81ce971d0c470b65ced3f07eb40ed
Avya04050 <71966607+Avya04050@users.noreply.github.com>
2020-12-05 22:47:20 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,26 @@
+class Box {
+  constructor(x, y, width, height, color) {
+    var options = {
+        'restitution':0.8,
+        'friction':1.0,
+        'density':0.5
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    this.color = color;
+    World.add(world, this.body);
+  }
+  display(){
+    //var angle = this.body.angle;
+    //var color = this.body.color;
+    push();
+    translate(this.body.position.x, this.body.position.y);
+    //rotate(angle);
+    rectMode(CENTER);
+    fill(this.color);
+    rect( 0, 0, this.width, this.height);
+    pop();
+  }
+
+};
